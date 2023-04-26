7dd6c81e9b52bf2b6ca71e5534151345fb892a69
Vynalith <53541704+Vynalith@users.noreply.github.com>
2023-04-26 14:42:04 -0400

    Statue works! :D
    Next up: fix the 100f force applied in the X direction on all projectiles

--- Assets/Scripts/Enemy/Statue/StatueShooter.cs
+++ Assets/Scripts/Enemy/Statue/StatueShooter.cs
@@ -5,38 +5,46 @@ using UnityEngine;
 public class StatueShooter : MonoBehaviour
 {
     public float cooldown;
-    private float cooldownCount;
+    public float cooldownCount;
     public GameObject Projectile;
     public float shotForce = 20f;
     public bool playerInRange;
     public GameObject aimer;
     public Animator animator;
+    Vector2 aimDirection;
+    Vector3 aimSpawner;
 
     //2 is down, 4 is left, 6 is right, 8 is up
-    public int direction;
+    //public int direction;
 
     // Start is called before the first frame update
     void Start()
     {
         cooldownCount = 0;
         playerInRange = false;
-        
+        aimDirection.x = 0f;
+        aimDirection.y = aimer.transform.position.y;
+        aimSpawner = this.transform.position;
+        aimSpawner.y = this.transform.position.y - 2f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerInRange=true)
+        if(playerInRange)
         {
            if (cooldownCount >= cooldown)
                 {
-                    //animator.Play("StatueDownSHOOT");
-
-                    Shoot();
-                    cooldownCount = 0;
-                } 
+                    animator.Play("StatueDownSHOOT");
+                    if (cooldownCount >= cooldown + .8f)
+                    {
+                        Shoot();
+                        cooldownCount = 0;
+                    }
+                }
+            
         }
-        
+        cooldownCount += Time.deltaTime;
     }
 
 
@@ -44,6 +52,7 @@ public class StatueShooter : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            print(other.gameObject);
             playerInRange= true;
         }
     }
@@ -63,7 +72,12 @@ public class StatueShooter : MonoBehaviour
 
         GameObject arrow = Instantiate(Projectile, this.transform.position, this.transform.rotation);
         Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
-        rb.AddForce(aimer.transform.position * shotForce * 25f);
+        //rb.AddForce(aimer.transform.position * shotForce);
+        //Vector3 aimdirection = (0f, aimer.transform.position.y, 0f);
+        //all projectiles have a built-in 100f force applied to X
+        aimDirection.x = -5f;
+        print(aimDirection);
+        rb.AddForce(aimDirection * shotForce);
 
     }
 
