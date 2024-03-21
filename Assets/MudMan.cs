50a5a663c7d05e021dd4e89777dc5aeab32624b4
Andrew Jorgensen <69862674+Zeta98159@users.noreply.github.com>
2024-03-21 13:58:17 -0700

    Revert "commit"
    This reverts commit 7be58ed9663474bb7113512cf4bdfca67989441e.

--- Assets/MudMan.cs
+++ Assets/MudMan.cs
@@ -14,7 +14,7 @@ public class MudMan : MonoBehaviour
     private Vector3 start;
     private Vector3 direction;
     private GameObject target;
-    private float sightDistance = 10;
+    public float sightDistance = 10;
     private Collider2D finalDetected;
     private RaycastHit hit;
     private int layerMask = 1 << 3 | 1 << 7 | 1 << 11 | 1 << 12 | 1 << 13;
@@ -24,24 +24,11 @@ public class MudMan : MonoBehaviour
     private Animator animator;
 
     private int heartOrNo;
-    private bool gameObject heart;
+    public bool GameObject heart;
     private float horizontal;
     private float vertical;
     private float stupidspeed;
-
-    public Vector3 Start { get => start; set => start = value; }
-    public Vector3 Direction { get => direction; set => direction = value; }
-    public GameObject Target { get => target; set => target = value; }
-    public global::System.Single SightDistance { get => sightDistance; set => sightDistance = value; }
-    public Collider2D FinalDetected { get => finalDetected; set => finalDetected = value; }
-    public RaycastHit Hit { get => hit; set => hit = value; }
-    public global::System.Int32 LayerMask { get => LayerMask1; set => LayerMask1 = value; }
-    public global::System.Int32 LayerMask1 { get => layerMask; set => layerMask = value; }
-    public Vector3 ShootAngle { get => shootAngle; set => shootAngle = value; }
-    public Animator Animator { get => animator; set => animator = value; }
-    public global::System.Int32 HeartOrNo { get => heartOrNo; set => heartOrNo = value; }
-    public global::System.Boolean GameObject { get => gameObject; set => gameObject = value; }
-}
+};
 // Start is called before the first frame update
 void Start()
 {
@@ -67,147 +54,22 @@ private int damage = 10;
         health = value;
     }
 
-public Animator GetAnimator()
-{
-    return animator;
-}
+    public Animator Animator { get => animator; set => animator = value; }
+    public global::System.Int32 HeartOrNo { get => heartOrNo; set => heartOrNo = value; }
+    public Vector3 ShootAngle { get => shootAngle; set => shootAngle = value; }
+    public RaycastHit Hit { get => hit; set => hit = value; }
+    public Collider2D FinalDetected { get => finalDetected; set => finalDetected = value; }
+    public Vector3 Direction { get => direction; set => direction = value; }
+    public Vector3 Start1 { get => start; set => start = value; }
+    public global::System.Single CooldownDuration { get => cooldownDuration; set => cooldownDuration = value; }
+    public global::System.Single Cooldown { get => cooldown; set => cooldown = value; }
+    public global::System.Single Horizontal { get => horizontal; set => horizontal = value; }
+    public global::System.Single Vertical { get => vertical; set => vertical = value; }
+    public global::System.Single Stupidspeed { get => stupidspeed; set => stupidspeed = value; }
+    public global::System.Int32 Damage { get => damage; set => damage = value; }
+    public GameObject HeartPickup { get => heartPickup; set => heartPickup = value; }
 
-public void SetAnimator(Animator value)
-{
-    animator = value;
-}
-
-public System.Int32 GetHeartOrNo()
-{
-    return heartOrNo;
-}
-
-public void SetHeartOrNo(System.Int32 value)
-{
-    heartOrNo = value;
-}
-
-public Vector3 GetShootAngle()
-{
-    return shootAngle;
-}
-
-public void SetShootAngle(Vector3 value)
-{
-    shootAngle = value;
-}
-
-public RaycastHit GetHit()
-{
-    return hit;
-}
-
-public void SetHit(RaycastHit value)
-{
-    hit = value;
-}
-
-public Collider2D GetFinalDetected()
-{
-    return finalDetected;
-}
-
-public void SetFinalDetected(Collider2D value)
-{
-    finalDetected = value;
-}
-
-public Vector3 GetDirection()
-{
-    return direction;
-}
-
-public void SetDirection(Vector3 value)
-{
-    direction = value;
-}
-
-public Vector3 GetStart1()
-{
-    return start;
-}
-
-public void SetStart1(Vector3 value)
-{
-    start = value;
-}
-
-public System.Single GetCooldownDuration()
-{
-    return cooldownDuration;
-}
-
-public void SetCooldownDuration(System.Single value)
-{
-    cooldownDuration = value;
-}
-
-public System.Single GetCooldown()
-{
-    return cooldown;
-}
-
-public void SetCooldown(System.Single value)
-{
-    cooldown = value;
-}
-
-public System.Single GetHorizontal()
-{
-    return horizontal;
-}
-
-public void SetHorizontal(System.Single value)
-{
-    horizontal = value;
-}
-
-public System.Single GetVertical()
-{
-    return vertical;
-}
-
-public void SetVertical(System.Single value)
-{
-    vertical = value;
-}
-
-public System.Single GetStupidspeed()
-{
-    return stupidspeed;
-}
-
-public void SetStupidspeed(System.Single value)
-{
-    stupidspeed = value;
-}
-
-public System.Int32 GetDamage()
-{
-    return damage;
-}
-
-public void SetDamage(System.Int32 value)
-{
-    damage = value;
-}
-
-public GameObject GetHeartPickup()
-{
-    return heartPickup;
-}
-
-public void SetHeartPickup(GameObject value)
-{
-    heartPickup = value;
-}
-
-private void Start()
+    private void Start()
 {
     cooldownCount = 0;
     target = GameObject.Find("Player");
@@ -215,7 +77,7 @@ private void Start()
     layerMask = ~layerMask;
 }
 
-private void Update()
+    private void Update()
     {
         cooldownCount -= Time.deltaTime;
 
@@ -228,48 +90,50 @@ private void Update()
         }
     }
 
-private float CalculateAngle(Vector3 from, Vector3 to) => Mathf.Atan2(to.y - from.y, to.x - from.x) * Mathf.Rad2Deg;
-
-private void Shoot(float angle)
-{
-    if (cooldownCount <= 0)
+    private float CalculateAngle(Vector3 from, Vector3 to)
     {
-        cooldownCount = Cooldown;
-
-        // Create a projectile at the shooter's position
-        GameObject projectile = Instantiate(Projectile, transform.position, Quaternion.identity);
-
-        // Calculate the direction vector
-        Vector3 direction = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
-
-        // Shoot the projectile in the direction vector
-        global::System.Object value = projectile.GetComponent<Rigidbody2D>()
-            .AddForce(direction * shotForce);
+        return Mathf.Atan2(to.y - from.y, to.x - from.x) * Mathf.Rad2Deg;
     }
-}
 
-private void OnTriggerEnter2D(Collider2D collision)
-{
-    if (!collision.gameObject.CompareTag("PlayerProjectile"))
+    private void Shoot(float angle)
     {
-        return;
+        if (cooldownCount <= 0)
+        {
+            cooldownCount = Cooldown;
+
+            // Create a projectile at the shooter's position
+            GameObject projectile = Instantiate(Projectile, transform.position, Quaternion.identity);
+
+            // Calculate the direction vector
+            Vector3 direction = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
+
+            // Shoot the projectile in the direction vector
+            projectile.GetComponent<Rigidbody2D>().AddForce(direction * shotForce);
+        }
     }
-    TakeDamage(10);
 
-    // Destroy the projectile
-    global::System.Object value = Destroy(collision.gameObject);
-}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerProjectile"))
+        {
+            TakeDamage(10);
 
-public void TakeDamage(int damage)
+            // Destroy the projectile
+            Destroy(collision.gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
     {
         health -= damage;
 
         if (health <= 0)
         {
-        // Trigger the game over condition
-        global::System.Object value1 = CurrentRoom.GameOver();
+            // Trigger the game over condition
+            CurrentRoom.GameOver();
 
-        // Change the current room
-        global::System.Object value = CurrentRoom.ChangeRoom();
+            // Change the current room
+            CurrentRoom.ChangeRoom();
         }
+    }
 }
