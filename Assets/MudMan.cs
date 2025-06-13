3f0dd4332707a9229f9e4e2eebfffdc7d7afba83
Andrew Jorgensen <andrainium@gmail.com>
2025-06-13 12:22:21 -0400

    Commit

--- Assets/MudMan.cs
+++ Assets/MudMan.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MudMan : MonoBehaviour
-{
+{   
 // Start is called before the first frame update
 void Start()
     health = 100;
