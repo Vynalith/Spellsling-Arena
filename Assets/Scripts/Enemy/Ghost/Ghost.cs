5f9fcf863c6e4b98faec0df4da30da396d5bcdf6
Andrew Jorgensen <andrainium@gmail.com>
2025-06-13 11:41:40 -0400

    commit

--- Assets/Scripts/Enemy/Ghost/Ghost.cs
+++ Assets/Scripts/Enemy/Ghost/Ghost.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ghost
+public class Ghost : MonoBehaviour
    {
     GameObject damage;
     GameObject currentRoom;
